1c405bb86ae44762f8df00539569f9a2626a2e00
Abuzar Younas Malik G <abuzaryounasmalikg@gmail.com>
2026-03-26 21:43:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -14,15 +14,18 @@ function clearScreen() {
 
 function solve() {
     try {
-        display.value = eval(display.value);
+       
+        if (display.value !== "") {
+            display.value = eval(display.value);
+        }
     } catch (err) {
         display.value = "Error";
     }
 }
 
-
 function changeBg() {
     let container = document.querySelector(".buttons");
-    let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    
+    let randomColor = `hsl(${Math.random() * 360}, 70%, 50%)`; 
     container.style.backgroundColor = randomColor;
 }
