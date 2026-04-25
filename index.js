8b4242f2bb919c308d6d388549778a0090e998ee
Abuzar Younas Malik G <abuzaryounasmalikg@gmail.com>
2026-04-25 14:00:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ function clearScreen() {
     display.value = "";
 }
 
+// Ye naya function aakhri character ko remove karta hai
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
+
 function solve() {
     try {
-       
         if (display.value !== "") {
             display.value = eval(display.value);
         }
@@ -25,7 +29,6 @@ function solve() {
 
 function changeBg() {
     let container = document.querySelector(".buttons");
-    
     let randomColor = `hsl(${Math.random() * 360}, 70%, 50%)`; 
     container.style.backgroundColor = randomColor;
 }
